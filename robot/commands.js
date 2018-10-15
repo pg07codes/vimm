d2aa9e533bfca2da99cf3b439a678fef9066c5d0
pranav gupta <pg07codes@gmail.com>
2018-10-15 19:04:05 +0530

    voice cmds working

--- robot/commands.js
+++ robot/commands.js
@@ -3,26 +3,36 @@
 const robot=require("robotjs")
 robot.setMouseDelay(1)
 
-
 module.exports={
     moveUp(val){
-
+        let X=robot.getMousePos().x
+        let Y=robot.getMousePos().y
+        robot.moveMouseSmooth(X,Y-val)
     },
     moveDown(val){
-
+        let X=robot.getMousePos().x
+        let Y=robot.getMousePos().y
+        robot.moveMouseSmooth(X,Y+val)
     },
     moveLeft(val){
-
+        let X=robot.getMousePos().x
+        let Y=robot.getMousePos().y
+        robot.moveMouseSmooth(X-val,Y)
     },
     moveRight(val){
-
+        let X=robot.getMousePos().x
+        let Y=robot.getMousePos().y
+        robot.moveMouseSmooth(X+val,Y)
     },
     click(){
-
+        robot.mouseClick()
     },
     rightClick(){
-
+        robot.mouseClick('right')
     },
+    doubleClick(){
+        robot.mouseClick('left',true)
+    }
 
 }
 
